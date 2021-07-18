618737c6f2680a7adbbd8729afc5e64bf92fa64e
Harry Thoma <thoma20@up.edu>
2021-07-18 14:14:44 -0700

    Add some missing tests

---
+++ src/test/java/com/semantax/ast/node/ModuleTest.java
@@ -0,0 +1,44 @@
+package com.semantax.ast.node;
+
+import com.semantax.ast.node.list.ModuleList;
+import com.semantax.ast.node.list.PatternDefinitionList;
+import com.semantax.ast.node.list.StatementList;
+import com.semantax.ast.util.FilePos;
+import junit.framework.TestCase;
+
+import java.util.List;
+
+public class ModuleTest extends TestCase {
+
+    public void test_subNodes() {
+        Statement first = Statement.builder().buildWith(new FilePos(1,1));
+        Module second = Module.builder().build();
+        second.setFilePos(new FilePos(2, 1));
+        Statement third = Statement.builder().buildWith(new FilePos(3,1));
+        PatternDefinition fourth = PatternDefinition.builder().buildWith(new FilePos(4, 1));
+
+        StatementList statements = new StatementList();
+        statements.add(first);
+        statements.add(third);
+
+        ModuleList modules = new ModuleList();
+        modules.add(second);
+
+        PatternDefinitionList patterns = new PatternDefinitionList();
+        patterns.add(fourth);
+
+        Module module = Module.builder()
+                    .statements(statements)
+                    .subModules(modules)
+                    .patterns(patterns)
+                    .build();
+
+        List<AstNode> subNodes = module.subNodes();
+
+        assertEquals(4, subNodes.size());
+        assertEquals(first, subNodes.get(0));
+        assertEquals(second, subNodes.get(1));
+        assertEquals(third, subNodes.get(2));
+        assertEquals(fourth, subNodes.get(3));
+    }
+}
--- src/test/java/com/semantax/ast/util/FilePosTest.java
+++ src/test/java/com/semantax/ast/util/FilePosTest.java
@@ -1,12 +1,32 @@
 package com.semantax.ast.util;
 
+import com.semantax.parser.generated.Token;
 import junit.framework.TestCase;
 
 public class FilePosTest extends TestCase {
 
     public void test_from() {
+        int beginLine = 42;
+        int beginColumn = 12;
+
+        Token token = new Token();
+        token.beginLine = beginLine;
+        token.beginColumn = beginColumn;
+
+        FilePos filePos = FilePos.from(token);
+
+        assertEquals(beginLine, filePos.getLine());
+        assertEquals(beginColumn, filePos.getColumn());
     }
 
     public void test_compareTo() {
+        FilePos pos10_4 = new FilePos(10, 4);
+        FilePos pos10_10 = new FilePos(10, 10);
+        FilePos pos20_4 = new FilePos(20, 4);
+
+        assertTrue(pos10_4.compareTo(pos10_10) < 0);
+        assertTrue(pos10_4.compareTo(pos20_4) < 0);
+        assertTrue(pos10_10.compareTo(pos20_4) < 0);
+        assertEquals(0, pos10_10.compareTo(pos10_10));
     }
 }
